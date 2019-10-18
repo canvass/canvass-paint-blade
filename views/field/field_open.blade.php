1e39660455e1decd39e3c4ff9cee56459345b437
Michael Kelly <puritandesigns@gmail.com>
2019-10-18 08:41:13 -0400

    Actually use wrap_classes

--- views/field/field_open.blade.php
+++ views/field/field_open.blade.php
@@ -1,6 +1,13 @@
-<div id="{{ $field['identifier'] }}_wrap" class="field-wrap">
+<div id="{{ $field['identifier'] }}_wrap"
+     class="{{ $field['wrap_classes'] ?? config(
+        'canvass.defaults.field.wrap_classes',
+        'field-wrap'
+     ) }}"
+>
 
-    <?php $hide_label ?? $hide_label = false; ?>
+    @php
+    $hide_label = $hide_label ?? false;
+    @endphp
 
     @if(! $hide_label)
         @include('canvass_paint::label/label')
