b00f52a7f4e2625747ec9bcbe6737cfe86fe67e1
Michael Kelly <puritandesigns@gmail.com>
2020-11-06 09:20:54 -0500

    Add radio view file

--- views/fieldset/fieldset.blade.php
+++ views/fieldset/fieldset.blade.php
@@ -1,7 +1,9 @@
 <fieldset id="{{ $field['identifier'] }}" class="{{ $field['wrap_classes'] ?? '' }}">
     <legend>{{ $field['label'] }}</legend>
 
-    @include('canvass_paint::help_text/help_text', compact('identifier','help_text'))
+    @if(isset($help_text))
+        @include('canvass_paint::help_text/help_text', compact('identifier','help_text'))
+    @endif
 
     <?php foreach($field['children'] ?? [] as $child): ?>
         @include("canvass_paint::{$child['general_type']}/{$child['general_type']}", [
